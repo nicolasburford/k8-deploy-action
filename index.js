203c78ff99fc840f1bf036003f44691d1e1ba73c
nicolasburford <burford.nicolas@gmail.com>
2020-05-03 13:14:27 -0400

    env variable

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const k8s = require('@kubernetes/client-node');
-const config = require('config');
+// const config = require('config');
 
 const main = async () => {
   const kc = new k8s.KubeConfig();
-  kc.loadFromString(Buffer.from(config.get('kubeConfig'), 'base64').toString('utf-8'));
+  kc.loadFromString(Buffer.from(process.env.KUBE_CONFIG_DATA, 'base64').toString('utf-8'));
   
   const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
   
